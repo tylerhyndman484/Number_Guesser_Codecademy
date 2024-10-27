ee2c6bc6f73e866eddb5bca0c7df920b843b794b
Tyler Hyndman <tylerhyndman484@hotmail.com>
2024-10-27 09:45:45 -0500

    working on number guesser project from codecademy

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+   return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (human, computer, secretTargetNumber) => {
+    const targetNumber = generateTarget();
+    const humanDifference = Math.abs(targetNumber - human);
+    const computerDifference = Math.abs(targetNumber - computer);
+    if (humanDifference < computerDifference || humanDifference === computerDifference) {
+        return true;
+    } else if (computerDifference < humanDifference) {
+        return false;
+    }
+}
