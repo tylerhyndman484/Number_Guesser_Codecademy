65453e5aa335808c330582721361ae5f15ad5264
Tyler Hyndman <tylerhyndman484@hotmail.com>
2024-10-28 07:59:59 -0500

    made a seperate function for the math of getting the distance

--- script.js
+++ script.js
@@ -8,13 +8,37 @@ const generateTarget = () => {
    return Math.floor(Math.random() * 10);
 }
 
-const compareGuesses = (human, computer, secretTargetNumber) => {
-    const targetNumber = generateTarget();
-    const humanDifference = Math.abs(targetNumber - human);
-    const computerDifference = Math.abs(targetNumber - computer);
-    if (humanDifference < computerDifference || humanDifference === computerDifference) {
+const getAbsoluteDistance = (number1, number2) => {
+    return Math.abs(number1 - number2);
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+    if (getAbsoluteDistance(humanGuess, targetNumber) < getAbsoluteDistance(computerGuess, targetNumber) || getAbsoluteDistance(humanGuess, targetNumber) === getAbsoluteDistance(computerGuess, targetNumber)) {
         return true;
-    } else if (computerDifference < humanDifference) {
+    } else if (getAbsoluteDistance(humanGuess, targetNumber) > getAbsoluteDistance(computerGuess, targetNumber)) {
         return false;
+    } 
+}
+
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
     }
 }
+
+const advanceRound = () => {
+    currentRoundNumber++
+}
+
+const userGuessError = () => {
+    const humanGuess = document.getElementById('human-guess');
+    if (humanGuess > 9 || humanGuess < 0) {
+        window.alert('Guess is out of range. Try number from 0 - 9.');
+    }
+}
+
+
+console.log(getAbsoluteDistance(9, 0)); //human
+console.log(getAbsoluteDistance(6, 0)); // computer
